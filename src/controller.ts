2d320b534c85c6b3d8d43e0ac875f55fda68ecf4
sirawat21 <sirawat21@gmail.com>
2022-12-21 00:53:03 +1100

    update function when coffeeType send by query param

--- src/controller.ts
+++ src/controller.ts
@@ -16,7 +16,7 @@ const model: RatedCoffeeType[] = [];
 router.post('/ratings', (req: Request, res: Response): void => {
    let { coffeeType, starRating } = req.body;
    // Set inital response
-   let responseMessage = { code: 400, message: 'Bad Request' };
+   let responseMessage = { code: 400, message: '400 Bad Request' };
    // Validating the request
    if ('coffeeType' in req.body && 'starRating' in req.body) {
       // Regular expression for starRating format
@@ -30,17 +30,36 @@ router.post('/ratings', (req: Request, res: Response): void => {
          model.push(ratedCoffeeType);
          // Set response message
          responseMessage.code = 201;
-         responseMessage.message = 'Created';
+         responseMessage.message = '201 Created';
       }
    }
    res.status(responseMessage.code).send(responseMessage);
 });
 
-// // [GET] - Get a message by id
-// router.get('/ratings/:coffeeType', (req: Request, res: Response) => {
-//    const coffeeType = req.params.coffeeType;
-//    res.status(200).send('');
-// });
+// [GET] - Get a message by coffee type
+router.get('/ratings', (req: Request, res: Response) => {
+   // Get query param of coffeeType
+   const coffeeType = req.query.coffeeType
+   // Set inital response
+   let responseMessage = { code: 400, message: '' };
+   // Validating the request
+   if (model.length > 0) {
+      // Get previous rated coffee type; access from the tail of array model
+      const previousRatedCoffeeType = model.slice(-1)[0];
+      // Check the matched value of coffee type between model and query param
+      if (coffeeType === previousRatedCoffeeType.coffeeType) {
+         // Set response message
+         responseMessage.code = 200;
+         responseMessage.message = '';
+      }
+   } else {
+      // When query param is not found from model
+      // Set spone message detail for not rating yet; coffeeType is from query param
+      const detailResponseMessage = { coffeeType: `${coffeeType} is not rated yet coffee type` };
+      responseMessage.message = JSON.stringify(detailResponseMessage);
+   }
+   res.status(responseMessage.code).send(responseMessage.message);
+});
 
 // [GET] - List all rating coffee types 
 router.get('/ratings/coffee-types', (req: Request, res: Response) => {
